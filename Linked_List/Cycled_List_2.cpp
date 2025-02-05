3300435a17fff668ec3436e17099d1f18470bb18
RANA DANISH <itzdanishpb65@gmail.com>
2025-02-05 12:41:40 +0500

    part-two

---
+++ Linked_List/Cycled_List_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class ListNode
+{
+public:
+    int val;
+    ListNode *next;
+};
+
+ListNode *partTwo(ListNode *head)
+{
+    ListNode *slow = head, *fast = head;
+    while (fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+        {
+            slow = head;
+            while (slow != fast)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+        }
+    }
+    return NULL;
+}
