6d113b827acb49c83ecaff0f17d3fdd60f79415e
RANA DANISH <itzdanishpb65@gmail.com>
2025-03-02 22:12:03 +0500

    3Sum

--- Arrays_Problem/Two_Sum.java
+++ Arrays_Problem/Two_Sum.java
@@ -6,22 +6,21 @@ public class Two_Sum {
     class Solution {
         public int[] twoSum(int[] nums, int target) {
             Map<Integer, Integer> map = new HashMap<>();
-
             for (int i = 0; i < nums.length; i++) {
                 int expectedElem = target - nums[i];
                 if (map.containsKey(expectedElem)) {
-                    return new int[]{map.get(expectedElem), i};
+                    return (new int[]{map.get(expectedElem), i});
                 }
                 map.put(nums[i], i);
             }
-            return null;
+            return new int[]{};
         }
     }
 
     public static void main(String[] args) {
-        Two_Sum two=new Two_Sum();
-        Two_Sum.Solution sol= two. new Solution();
-        int[] nums= new int[]{2, 7, 11, 15};
+        Two_Sum two = new Two_Sum();
+        Two_Sum.Solution sol = two.new Solution();
+        int[] nums = new int[]{2, 7, 11, 15};
         System.out.println(Arrays.toString(sol.twoSum(nums, 9)));
 
     }
